8622e4231624e5b29e34bfe19a4d0bb9094b9762
Apple228 <alekse.lazarev.01@gmail.com>
2022-05-11 16:32:35 +0300

    перед добавлением вопроса Кол-во показов Барн 10

--- handlers/users/gsheets_new.py
+++ handlers/users/gsheets_new.py
@@ -15,18 +15,22 @@ from keyboards.default.numbers_for_gsheets import numbers
 from keyboards.inline.gsheets_timer import gsheets_timer_finished_house
 from loader import dp, db
 
+
 @dp.message_handler(Command("data_new"))
 async def update_data_gsheet_finished_house(dp: Dispatcher):
     for user_gsheets in DEPARTMENT_OF_READY_HOUSES:
         if (await db.check_gsheets_today(telegram_id=user_gsheets) == 0):
-            await dp.bot.send_message(user_gsheets, "Ежедневный сбор статистики в таблицу", reply_markup=gsheets_timer_finished_house)
+            await dp.bot.send_message(user_gsheets, "Ежедневный сбор статистики в таблицу",
+                                      reply_markup=gsheets_timer_finished_house)
             username = await db.select_full_name(user_gsheets)
             await dp.bot.send_message(624523030, f"Сообщение отправлено для {username}")
+
 
 @dp.callback_query_handler(text="Отдел готовых домов")
 async def state_data_gsheets(call: CallbackQuery, state: FSMContext):
     await call.message.answer("Заинтересовал Барн 7", reply_markup=numbers)
     await state.set_state("Заинтересовал Барн 7")
+
 
 @dp.message_handler(state="Заинтересовал Барн 7")
 async def state_data_gsheets(message: types.Message, state: FSMContext):
@@ -39,6 +43,7 @@ async def state_data_gsheets(message: types.Message, state: FSMContext):
         await message.answer("Введено не целое число, давай ещё раз")
         await state.set_state("Заинтересовал Барн 7")
 
+
 @dp.message_handler(state="Заинтересовал Барн 10")
 async def state_data_gsheets(message: types.Message, state: FSMContext):
     interested_in_Barn10 = message.text
@@ -49,6 +54,7 @@ async def state_data_gsheets(message: types.Message, state: FSMContext):
     except:
         await message.answer("Введено не целое число, давай ещё раз")
         await state.set_state("Заинтересовал Барн 10")
+
 
 @dp.message_handler(state="Заинтересовал Шведский 2")
 async def state_data_gsheets(message: types.Message, state: FSMContext):
@@ -61,7 +67,6 @@ async def state_data_gsheets(message: types.Message, state: FSMContext):
     except:
         await message.answer("Введено не целое число, давай ещё раз")
         await state.set_state("Заинтересовал Шведский 2")
-
 
 
 @dp.message_handler(state="Заинтересовал другой проект")
@@ -82,13 +87,13 @@ async def state_data_gsheets(message: types.Message, state: FSMContext):
     number_of_impressions_in_the_sale_of_houses = message.text
     try:
 
-        await state.update_data(number_of_impressions_in_the_sale_of_houses=int(number_of_impressions_in_the_sale_of_houses))
+        await state.update_data(
+            number_of_impressions_in_the_sale_of_houses=int(number_of_impressions_in_the_sale_of_houses))
         await message.answer("Кол-во показов в стройку", reply_markup=numbers)
         await state.set_state("Кол-во показов в стройку")
     except:
         await message.answer("Введено не целое число, давай ещё раз")
         await state.set_state("Кол-во показов в продажу домов")
-
 
 
 @dp.message_handler(state="Кол-во показов в стройку")
@@ -105,7 +110,6 @@ async def save_data_gsheets(message: types.Message, state: FSMContext):
     client = await client.authorize()
     async_spreadsheet = await client.open_by_key(spreadsheet_id)
 
-
     values = []
     today = datetime.date.today()
     data_time = today.strftime('%d.%m.%y')
@@ -115,7 +119,6 @@ async def save_data_gsheets(message: types.Message, state: FSMContext):
     values.append(int(data.get("interested_in_Barn7")))
     values.append(int(data.get("interested_in_Barn10")))
     values.append(int(data.get("interested_in_Swedish2")))
-
 
     # values.append(data.get("interested_in_Barn6"))
     # values.append(data.get("interested_in_Barn3"))
@@ -127,17 +130,50 @@ async def save_data_gsheets(message: types.Message, state: FSMContext):
     logging.info(values)
     await dp.bot.send_message(624523030, f"{values}")
     await dp.bot.send_sticker(message.from_user.id,
-                              'CAACAgIAAxkBAAIQLWEk1jSoIIEBvFImzG43r2pSNer3AAIQDwACQvzYS_tsLq_GpJXBIAQ', reply_markup=menu)
+                              'CAACAgIAAxkBAAIQLWEk1jSoIIEBvFImzG43r2pSNer3AAIQDwACQvzYS_tsLq_GpJXBIAQ',
+                              reply_markup=menu)
     worksheet = await async_spreadsheet.worksheet('БОТ ГД3')
     await worksheet.append_row(values)
     await state.reset_state()
     await db.update_gsheets_today(telegram_id=message.from_user.id)
+
 
 @dp.message_handler(text="Напоминание про таблицу")
 async def check_gsheet_finished_house(message: types.Message):
     # logging.info(await db.check_gsheets_today(telegram_id=message.from_user.id))
     for user_gsheets in DEPARTMENT_OF_READY_HOUSES:
         if (await db.check_gsheets_today(telegram_id=user_gsheets) == 0):
-            await dp.bot.send_message(user_gsheets, "Напоминаю, что нужно заполнить статистику", reply_markup=gsheets_timer_finished_house)
+            await dp.bot.send_message(user_gsheets, "Напоминаю, что нужно заполнить статистику",
+                                      reply_markup=gsheets_timer_finished_house)
             username = await db.select_full_name(user_gsheets)
             await dp.bot.send_message(624523030, f"Таблицу не заполнил {username}")
+
+
+@dp.callback_query_handler(text="Заполнить нулями готовые дома")
+async def state_data_gsheets(call: CallbackQuery, state: FSMContext):
+    spreadsheet_id = '1hocu-OWJdIDiTmy1WlteqprXhYPn7sIKkNUi8vdjXfQ'
+    client = gspread_asyncio.AsyncioGspreadClientManager(get_scoped_credentials(PATH))
+    client = await client.authorize()
+    async_spreadsheet = await client.open_by_key(spreadsheet_id)
+    # worksheet = await add_worksheet(async_spreadsheet, 'Лист2')
+    await dp.bot.send_sticker(call.from_user.id,
+                              "CAACAgIAAxkBAAIQJmEk1FkF6Cp75JJwbDSwKW1j7e8LAAJaDwACg1TYS2Vw3nymTXs9IAQ")
+    worksheet = await async_spreadsheet.worksheet('БОТ ГД3')
+
+    values = []
+
+    today = datetime.date.today()
+    data_time = today.strftime('%d.%m.%y')
+
+    values.append(data_time)
+    values.append(call.from_user.first_name + " " + call.from_user.last_name)
+    values.append(0)
+    values.append(0)
+    values.append(0)
+    values.append(0)
+    values.append(0)
+    values.append(0)
+    logging.info(values)
+    await dp.bot.send_message(624523030, f"{values}")
+    await worksheet.append_row(values)
+    await db.update_gsheets_today(telegram_id=call.from_user.id)
