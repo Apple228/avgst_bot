7c04611b7459be616b990e091351e558d7d4d78d
avlazarev <alekse.lazarev.01@gmail.com>
2023-07-27 01:23:07 +0300

    добавление новых вопросов и подсчет статистики

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -8,5 +8,7 @@ async def set_default_commands(dp):
         types.BotCommand("help", "Помощь"),
         types.BotCommand("menu", "Возврат в меню"),
         types.BotCommand("cancel", "Отмена"),
+        types.BotCommand("/get_stats_today", "Подсчет статистики за сегодня"),
+
 
     ])
